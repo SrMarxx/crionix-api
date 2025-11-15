b3b708a9bf50bd45b4c1c0f6a26af0503eba98a7
SrMarxx <marcosgabrieldiascardoso66@gmail.com>
2025-11-15 02:42:54 -0300

    initial commit

---
+++ src/main/java/br/com/smarttech/crionix/CrioNixApplication.java
@@ -0,0 +1,13 @@
+package br.com.smarttech.crionix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrioNixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrioNixApplication.class, args);
+	}
+
+}
